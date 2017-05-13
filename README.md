ad9caa2b43df5d82e811ff6aa154196a8b31d8fd
Brian W <brian.weinfeld@gmail.com>
2017-05-13 17:50:46 -0400

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# csv_to_sqlite
-Quick Conversion from CSV to SQLite to ease further data parsing
+<h1>CSV TO SQL</h1>
+<h2>Brian W Projects</h2>
+
+This small program will parse through a .csv file and easily convert it to a SQLite file for further easy querying.
+
+Usage:
+csv_to_sqlite.py [-h] [-i] [-t TABLE_NAME] [-d DATABASE] filename
+
+Convert CSV file to SQLITE table
+
+positional arguments:
+  filename       Set .csv file to read from.
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -i             Set flag to identify Column types otherwise program will
+                 attempt to determine automatically.
+  -t TABLE_NAME  Set table name. Default is data.
+  -d DATABASE    Set existing database to add table to. Default creates new
+                 database named data.db
+
+
--- csv_to_sqlite.py
+++ csv_to_sqlite.py
@@ -3,118 +3,118 @@ import sys
 import sqlite3
 import argparse
 
-db = sqlite3.connect('data.db')
-c = db.cursor()
 
-
-def read_csv(filename, define):
-    if not filename.endswith('.csv'):
-        filename += '.csv'
-    reader = csv.reader(open(filename))
-
-    list_reader = list(reader)
-
-    if define:
-        print('Identify each column as:\nText: t\nInteger: i\nBlob: b\nReal: r\n')
-        info = [(x, convert_input_data(input(x + ' is of type: '))) for x in list_reader[0]]
-
-        modify = []
-        for row in list_reader[1:]:
-            modify.append([convert_types(ele, typping[1]) for ele, typping in zip(row, info)])
-
-        return info, modify
-    else:
-        column_names = [name for name in list_reader[0]]
-        column_data_types = [find_types(column_data) for column_data in list_reader[1]]
-
-        info = [(name, data_type) for name, data_type in zip(column_names, column_data_types)]
-
-        modify = []
-        for row in list_reader[1:]:
-            modify.append([convert_types(ele, typping) for ele, typping in zip(row, column_data_types)])
-
-        return info, modify
-
-
-
-
-def find_types(x):
+def find_types(head_ele, column_data):
     try:
-        int(x)
+        int(column_data)
+        print(head_ele + ' is INTEGER')
         return 'INTEGER'
-    except ValueError as v:
+    except ValueError:
         pass
     try:
-        float(x)
+        float(column_data)
+        print(head_ele + ' is REAL')
         return 'REAL'
-    except ValueError as v:
+    except ValueError:
         pass
-    if x.startswith('$'):
+    if column_data.startswith('$'):
+        print(head_ele + ' is REAL')
         return 'REAL'
-    else:
-        try:
-            str(x)
-            return 'TEXT'
-        except ValueError as v:
-            return 'BLOB'
+    try:
+        str(column_data)
+        print(head_ele + ' is TEXT')
+        return 'TEXT'
+    except ValueError:
+        print(head_ele + ' is BLOB')
+        return 'BLOB'
 
 
-def convert_types(x, y):
-    if y == 'INTEGER':
-        return int(x)
-    if y == 'REAL':
-        if x.startswith('$'):
-            x = x.replace('$', '')
-            x = x.replace(',', '')
-        return float(x)
-    if y == 'TEXT':
-        return str(x)
-    if y == 'BLOB':
-        return str(x)
+def convert_types(element, typing):
+    try:
+        if typing == 'INTEGER':
+            return int(element)
+        if typing == 'REAL':
+            if element.startswith('$'):
+                element = element.replace('$', '')
+                element = element.replace(',', '')
+            return float(element)
+        if typing == 'TEXT':
+            return str(element)
+        if typing == 'BLOB':
+            return str(element)
+    except ValueError:
+        print('"' + element + '" is not of type ' + typing + '.')
+        print('Exiting...')
+        sys.exit(1)
 
 
 def convert_input_data(input_data):
     return {'t': 'TEXT',
             'i': 'INTEGER',
             'b': 'BLOB',
-            'r': 'REAL'}.get(input_data, input_data)
+            'r': 'REAL'}.get(input_data, 'BLOB')
 
 
 def create_table(header, table_name):
-    c.execute('SELECT name FROM sqlite_master WHERE type = "table" AND name="'+table_name+'"')
-    if c.fetchone() is not None:
-        print('This table already exists.')
-        sys.exit(1)
-
-    creation = 'CREATE TABLE IF NOT EXISTS '+ table_name + '('
+    creation = 'CREATE TABLE IF NOT EXISTS ' + table_name + '('
     for var_name, var_type in header:
         creation += var_name + ' ' + var_type + ', '
     creation = creation[0:-2] + ')'
-    print(creation)
     c.execute(creation)
 
 
-def add_data(header, data, tablename):
-    creation = 'INSERT INTO ' + tablename + ' VALUES(' + ('?, ' * len(header))
+def add_data(data, tablename):
+    creation = 'INSERT INTO ' + tablename + ' VALUES(' + ('?, ' * len(data))
     creation = creation[0:-2] + ')'
-
-    c.executemany(creation, data)
+    c.execute(creation, data)
     db.commit()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert CSV file to SQLITE table')
-    parser.add_argument('-d', action='store_true', dest='define', default=False,
+    parser.add_argument('-i', action='store_true', dest='define', default=False,
                 help='Set flag to identify Column types otherwise program will attempt to determine automatically.')
     parser.add_argument('filename', action='store', help='Set .csv file to read from.')
     parser.add_argument('-t', action='store', default='data', dest='table_name',
                 help='Set table name. Default is data.')
+    parser.add_argument('-d', action='store', default='data.db', dest='database',
+                help='Set existing database to add table to. Default creates new database named data.db')
 
     results = parser.parse_args()
     filename = results.filename
     define = results.define
-    table_name = results.table_name or 'table'
+    table_name = results.table_name or 'data'
+    database = results.database or 'data.db'
 
-    header, data = read_csv(filename, define)
-    create_table(header, table_name)
-    add_data(header, data, table_name)
+    if not filename.endswith('.csv'):
+        filename += '.csv'
+    if not database.endswith('.db'):
+        database += '.db'
+    print('Adding data to database "' + database + '", table "' + table_name + '", from file "' + filename + '"')
+
+    db = sqlite3.connect(database)
+    c = db.cursor()
+
+    c.execute('SELECT name FROM sqlite_master WHERE type = "table" AND name="'+table_name+'"')
+    if c.fetchone() is not None:
+        print('Table "' + table_name + '" already exists.')
+        print('Exiting without adding entries')
+        sys.exit(1)
+
+    reader = csv.reader(open(filename))
+    row0 = next(reader)
+
+    if define:
+        print('Identify each column as:\nText: t\nInteger: i\nBlob: b\nReal: r\n')
+        header_info = [(head_ele, convert_input_data(input(head_ele + ' is of type: '))) for head_ele in row0]
+        create_table(header_info, table_name)
+    else:
+        print('Identifying variable types')
+        row1 = next(reader)
+        header_info = [(head_ele, find_types(head_ele, column_data)) for head_ele, column_data in zip(row0, row1)]
+        create_table(header_info, table_name)
+        add_data([convert_types(element, typing[1]) for element, typing in zip(row1, header_info)], table_name)
+
+    for row in reader:
+        add_data([convert_types(element, typing[1]) for element, typing in zip(row, header_info)], table_name)
+    print('Data successfully added')
